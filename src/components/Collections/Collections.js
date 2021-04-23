e8bdb89cd3edd4b4bdcfd3ab9b5b5fff3d0559be
Marika <marikashanahan@gmail.com>
2021-04-23 01:06:16 -0600

    Write helper function to error handle GET fetch and apply to Collections component

--- src/components/Collections/Collections.js
+++ src/components/Collections/Collections.js
@@ -1,8 +1,16 @@
+import getOembed from '../../api-calls';
+
 const Collections = ({ toRender }) => {
+
+  const cleanGetReq = (singleUrl) => {
+    getOembed(singleUrl)
+      .then(result => result.html)
+      .catch(error => console.log(error))
+  }
 
   return (
     <section>
-      
+      { cleanGetReq(toRender.urls[0]) }
     </section>
   )
 };
