437b26de74c981eb30a23c4dbae4c084d326726c
Marika <marikashanahan@gmail.com>
2021-04-23 00:41:32 -0600

    Create Collections component files and import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import Collections from '../Collections/Collections';
+
 const App = () => {
   return (
     <div className="App">
-      
+      <main>
+        <input type="text" />
+        <Collections />
+      </main>
     </div>
   );
 }
---
+++ src/components/Collections/Collections.js
@@ -0,0 +1,8 @@
+const Collections = () => {
+
+  return (
+    <div></div>
+  )
+};
+
+export default Collections;
