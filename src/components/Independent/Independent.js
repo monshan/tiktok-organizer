ce5dd88594eb5b9340682f0e39bf6f41017d9377
Marika <marikashanahan@gmail.com>
2021-04-26 15:06:12 -0600

    Render CardContent if server error on UI

--- src/components/Independent/Independent.js
+++ src/components/Independent/Independent.js
@@ -1,7 +1,7 @@
 import { 
   Grid,
   Card,
-  // CardContent,
+  CardContent,
   CardMedia,
   CardActions,
   // Typography,
@@ -49,7 +49,7 @@ const Independent = ({ tiktoksrc, addPin, removePin }) => {
       sm={4}
       xs={6}
     >
-      {error && }
+      {error && <CardContent>Cannot retrieve from TikTok!</CardContent>}
       <Card
         elevation={6}
       >
