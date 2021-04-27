413071035f131e563e8dad51757c6ca1382c4a3f
Marika <marikashanahan@gmail.com>
2021-04-27 02:32:15 -0600

    Rewrite fixtures

--- cypress/integration/tttracker.spec.js
+++ cypress/integration/tttracker.spec.js
@@ -1,51 +1,67 @@
 describe('TTTracker', () => {
   describe('Bad Paths', () => {
-    // it('Should display server error', () => {
-    //   cy.intercept({
-    //     method: 'GET',
-    //     url: '/'
-    //   },{
-    //     statusCode: 200,
-    //     status_msg: 'Something went wrong'
-    //   })
-    //   cy.visit('http://localhost:3000/')
-    //   cy.get('[forCypress=card-error]').should('exist')
-    //     .contains('Something went wrong, Cannot retrieve from TikTok!').and('be.visible')
-    // })
+    it('Should display server error', () => {
+      cy.intercept({
+        method: 'GET',
+        url: '/'
+      },
+      { headers: {
+        "content-type": 'text/html',
+        fixture: "serverError.json"
+      }
+      })
+
+      // cy.intercept(
+      //   { method: "GET", url: "/bankAccounts" },
+      //   {
+      //     headers: {
+      //       "access-control-allow-origin": window.location.origin,
+      //       "Access-Control-Allow-Credentials": "true",
+      //     },
+      //     body: { results: [] },
+      //   }
+      // );
+
+      cy.visit('http://localhost:3000')
+      cy.get('[for-cypress=card-error]').should('exist')
+        .contains('Something went wrong, Cannot retrieve from TikTok!').and('be.visible')
+    })
   
     it('Should display incorrect path error', () => {
       cy.visit('http://localhost:3000/any-invalid-route')
-      cy.get('[forCypress=bad-route]')
+      cy.get('[for-cypress=bad-route]')
         .contains('This page doesn\'t exist! Please navigate back to Home with the Home tab above~')
         .and('be.visible')
     })
   })
 
-  beforeEach(() => {
-    cy.intercept('/', { fixture: "forIndependent.json" })
-    cy.visit('http://localhost:3000');
-   });
+  
 
-   describe('CRUD Operations', () => {
-     it('Can display accurate Cards from App state', () => {
+  describe('CRUD Operations', () => {
+    beforeEach(() => {
+      // cy.intercept('/', { fixture: "forIndependent.json" })
+      cy.visit('http://localhost:3000');
+    });
 
-     })
+    it('Can display accurate Cards from App state', () => {
+      cy.get('[for-cypress=home-grid]').should('have.length', 1)
+        
+    })
 
-     it('Can use the controlled form to add a tiktok card to the home page', () => {
-
-     })
+    it('Can use the controlled form to add a tiktok card to the home page', () => {
+      cy.get('')
+    })
      
-     it('Can remove a tiktok card from the home page', () => {
+    it('Can remove a tiktok card from the home page', () => {
 
-     })
+    })
 
-     it('Can bookmark a tiktok card', () => {
+    it('Can bookmark a tiktok card', () => {
 
-     })
+    })
 
-     it('Can remove a bookmark from a tiktok card', () => {
+    it('Can remove a bookmark from a tiktok card', () => {
 
-     })
-
-   })
+    })
+  })
 })
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -64,6 +64,7 @@ const Home = ({ initTikToks, removeTikTok }) => {
       justify="center"
       alignItems="center"
       children={ renderAsCards() }
+      for-cypress="home-grid"
     />
   )
 }
