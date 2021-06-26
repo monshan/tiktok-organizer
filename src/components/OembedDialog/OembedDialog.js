c9a56acbdfe117c25259bf1123d2fd7798b2b4f0
Marika <marikashanahan@gmail.com>
2021-06-26 02:43:58 -0600

    Remove parser for deployment pass

--- src/components/OembedDialog/OembedDialog.js
+++ src/components/OembedDialog/OembedDialog.js
@@ -1,8 +1,8 @@
 import { Dialog, DialogActions, DialogContent, Button } from '@material-ui/core';
 // import { useEffect, useState } from 'react';
-import parse from 'html-react-parser';
+// import parse from 'html-react-parser';
 
-const OembedDialog = ({ indDialog, toggleDialog, content }) => {
+const OembedDialog = ({ indDialog, toggleDialog }) => {
   return (
     <Dialog open={ indDialog }>
       <DialogContent>
